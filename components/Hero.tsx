b3b165685eae4ab030c8ffda9dea37e8e5da8d73
Түвшин Эрдэнэбат <24LP0845@G401-CC0091.local>
2024-10-02 13:04:37 +0800

    bolson

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import Avatar from "./components/Avatar";
+import Menu from "./components/Menu";
 import { Spotlight } from "./ui/Spotlight";
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-      <div>
+      <div className="flex justify-end  items-center">
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
           fill="white"
@@ -17,7 +18,15 @@ const Hero = () => {
           w-[50vw]"
           fill="blue"
         />
+
+        <Menu />
       </div>
+      <div>
+        <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] relative flex items-center justify-center">
+          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+        </div>
+      </div>
+      <div className="flex justify-center items-center"></div>
     </div>
   );
 };
