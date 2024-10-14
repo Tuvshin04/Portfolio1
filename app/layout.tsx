281ea1355ff8a383dceaf6991cfe2def5e0e67aa
Түвшин Эрдэнэбат <24LP0845@G401-CC0091.local>
2024-10-14 09:37:04 +0800

    zasvar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+
 import "./globals.css";
 import { ThemeProvider } from "./provider";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Tuvshin's Portfolio",
@@ -17,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,10 @@
-import Avatar from "./components/Avatar";
 import Menu from "./components/Menu";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+
 const Hero = () => {
+  const words = "Hello i am Tuvshin";
+  const words1 = "I'am Software Engineer ";
   return (
     <div className="pb-20 pt-36">
       <div className="flex justify-end  items-center">
@@ -18,15 +21,32 @@ const Hero = () => {
           w-[50vw]"
           fill="blue"
         />
-
-        <Menu />
+        {/* <Menu /> */}
       </div>
       <div>
-        <div className="h-screen w-full dark:bg-black bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] relative flex items-center justify-center">
-          <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+        <div className="h-screen">
+          <div className=" w-full dark:bg-black bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
+            <div className=" absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+          </div>
+          <div className="max-w-[89vw] md:max-w-20 lg:max-w[60vw] flex flex-col justify-center items-start ">
+            <h1 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 lg:max-w-[400px] lg:flex lg:justify-start lg:flex-col lg:items-center">
+              <TextGenerateEffect
+                duration={1}
+                filter={true}
+                words={words}
+                className="text-center text-[40px] lg:w-[300px] md:w-[300px]"
+              />
+              <TextGenerateEffect
+                duration={1}
+                filter={true}
+                words={words1}
+                className="text-center text-[40px] lg:w-[300px] md:w-[300px]"
+              />
+            </h1>
+          </div>
         </div>
       </div>
-      <div className="flex justify-center items-center"></div>
+      <div className="flex flex-col justify-center items-center relative my-20 z-10  "></div>
     </div>
   );
 };
