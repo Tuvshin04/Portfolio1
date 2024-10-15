b4a05d1a5d98ddcf8d404db68b1cb7809c18f719
Түвшин Эрдэнэбат <24LP0845@G401-CC0091.local>
2024-10-15 11:37:11 +0800

    about me heseg

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
   const words2 =
     "  Hello, my name is Tuvshin, I am 18 years old, I graduated from the 38th German school in the summer of 2024. Then I start studying at Pinecone Academy";
   return (
-    <div className="pb-20 pt-36 h-[4000px]">
+    <div className="pb-20 pt-8 h-fit">
       <div className="flex justify-end  items-center">
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
@@ -34,7 +34,7 @@ const Hero = () => {
         <Menu />
       </div>
       <div className="mt-[200px]  ">
-        <div className="h-screen">
+        <div className="h-fit">
           <div className=" w-full dark:bg-black bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
             <div className=" absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
           </div>
@@ -60,10 +60,10 @@ const Hero = () => {
             </div>
           </div>
           <div>
-            <div className="flex justify-center items-center ">
+            <div className="flex justify-center items-center mt-[200px] ">
               <h3 className="text-2xl font-thin text-blue-100">About Me</h3>
             </div>
-            <div className="flex justify-around items-center mt-[100px] ">
+            <div className="flex justify-around items-center mt-[10px] ">
               <div>
                 <TextGenerateEffect
                   duration={1}
@@ -71,23 +71,28 @@ const Hero = () => {
                   words={words2}
                   className="uppercase tracking-widest text-xl  text-white max-w-80 lg:max-w-[400px] lg:flex lg:justify-start lg:flex-col lg:items-center"
                 />
+                <div className="flex justify-start gap-5 items-center mt-[30px]">
+                  <div className="avatar">
+                    <div className="w-[100px] rounded-full">
+                      <img src="https://instagram.fuln8-1.fna.fbcdn.net/v/t1.15752-9/462548464_1053133256078031_5498301594125719827_n.jpg?stp=dst-jpg_s403x403&_nc_cat=104&ccb=1-7&_nc_sid=0024fc&_nc_ohc=pDyjVaztBdQQ7kNvgEIytmM&_nc_zt=23&_nc_ht=instagram.fuln8-1.fna&_nc_gid=ADxPwHg-K_2TZnBegNr6LKh&oh=03_Q7cD1QHpmjr8Gy_bZkpPQUz-iBl6bDCOFIdTV8Ku2iGLmOhTCw&oe=67350DD8" />
+                    </div>
+                  </div>
+                  <div className="border-2 w-[200px] border-blue-500"></div>
+                  <div className="avatar">
+                    <div className="w-[100px] rounded-full">
+                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4CiTFpFgtgX6gfMv1ILNcPgDic__3g2ukVw&s" />
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="w-[700px]">
+              <div className="w-[600px]">
                 <GlobeDemo />
               </div>
             </div>
-            <div className="flex justify-center gap-36 items-center mt-[50px]">
-              <div className="avatar">
-                <div className="w-[100px] rounded-full">
-                  <img src="https://instagram.fuln8-1.fna.fbcdn.net/v/t1.15752-9/462548464_1053133256078031_5498301594125719827_n.jpg?stp=dst-jpg_s403x403&_nc_cat=104&ccb=1-7&_nc_sid=0024fc&_nc_ohc=pDyjVaztBdQQ7kNvgEIytmM&_nc_zt=23&_nc_ht=instagram.fuln8-1.fna&_nc_gid=ADxPwHg-K_2TZnBegNr6LKh&oh=03_Q7cD1QHpmjr8Gy_bZkpPQUz-iBl6bDCOFIdTV8Ku2iGLmOhTCw&oe=67350DD8" />
-                </div>
-              </div>
-              <div className="border-2 w-[200px] border-blue-500"></div>
-              <div className="avatar">
-                <div className="w-[100px] rounded-full">
-                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4CiTFpFgtgX6gfMv1ILNcPgDic__3g2ukVw&s" />
-                </div>
-              </div>
+          </div>
+          <div>
+            <div className="flex justify-center items-center mt-[200px]">
+              <h3 className="text-2xl font-thin text-blue-100">Skills</h3>
             </div>
           </div>
         </div>
